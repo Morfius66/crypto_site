fd0f0114d9527b851504cf692e8a267f8fc04c6b
Muslim <grasss061@gmail.com>
2024-12-25 02:57:05 +0300

    Updated port for Railway

--- app.py
+++ app.py
@@ -35,4 +35,5 @@ def coin_details(coin_id):
     return render_template('coin.html', coin=coin_data)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host="0.0.0.0", port=8080)
+
